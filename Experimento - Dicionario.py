c998ee02de465fbab8b74193d32ccb668904bb8a
Mateus Sátiro <mateus_sat@hotmail.com>
2017-12-19 17:04:12 -0200

    Experimentos
    fazendo experimentos usando dicionarios

---
+++ Experimento - Dicionario.py
@@ -0,0 +1,34 @@
+#===================================#
+#     EXPERIMENTO COM DICIONARIO    #
+#===================================#
+
+# tem 2 elementos dentro do dicionario
+# o primeiro e a chave(item) e o segundo o valor(valor do item)
+# dentro do valor voce pode colocar "strings", "numeros", "listas", dentro das listas pode ter outros dicionarios...
+
+
+dicionario = {'chave': 'valor1', 'chave2': 'valor'}
+
+#print dicionario
+#print len(dicionario)
+
+#com o dicionario voce acessa um item da lista pelo nome
+
+#print dicionario['chave2']
+
+#posso mandar imprimir as chaves que existem
+
+print dicionario.keys()
+
+# assim como posso imprimir os valores que existem
+
+print dicionario.values()
+
+
+
+
+
+
+
+
+
